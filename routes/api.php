87e1d2431f3baac3a22f68c35edb04005b1a42b2
Wanderlan <wanderlanribeirodasilva51@gmail.com>
2023-11-26 18:04:56 -0300

    add more routes

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/greeting', function () {
-    return 'Hello World';
+    return 'Hello World...';
 });
